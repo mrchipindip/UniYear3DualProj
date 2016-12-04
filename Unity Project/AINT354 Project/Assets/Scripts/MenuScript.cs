1820350338b1d6ed906627c7d7fd06be8a8fc1a2
Sam James Booty <sam.booty@students.plymouth.ac.uk>
2016-12-04 18:52:43 +0000

    Added main menu, added splash screen (needs some more work) removed the sowcase scene so we can start work on the first actual level, also added twitter/website photos, 3ds max models, and the fbx files for the following assests: bin, cage and boards. Added the poster to the website photos.

---
+++ Unity Project/AINT354 Project/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+	public Canvas quitMenu;
+	public Button playText;
+	public Button quitText;
+
+
+	// Use this for initialization
+	void Start () {
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		playText = playText.GetComponent<Button> ();
+		quitText = quitText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+	}
+
+	public void ExitPress() {
+		quitMenu.enabled = true;
+		playText.enabled = false;
+		quitText.enabled = false;
+	}
+
+	public void NoPress() {
+		quitMenu.enabled = false;
+		playText.enabled = true;
+		quitText.enabled = true;
+	}
+
+	public void StartGame() {
+		SceneManager.LoadScene ("FirstLevel");
+	}
+
+	public void StartTutorial() {
+		//SceneManager.LoadScene ("");
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+	
+}
---
+++ Unity Project/AINT354 Project/Assets/Scripts/WaitTimeThenLoadLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WaitTimeThenLoadLevel : MonoBehaviour {
+	public float timer = 2f;
+	public string levelToLoad = "Level1";
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("DisplayScene");
+	}
+	
+	IEnumerator DisplayScene() {
+		yield return new WaitForSeconds (timer);
+		SceneManager.LoadScene(levelToLoad);
+	}
+}
