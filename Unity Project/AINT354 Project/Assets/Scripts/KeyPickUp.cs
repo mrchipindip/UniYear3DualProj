c547c8d99e7c228ef06846cb9ec3398f4a169ff7
Chris Wesemael <chrislbw@hotmail.co.uk>
2016-12-16 18:43:19 +0000

    Added script for object noises on collision with the floor. Added the half of the game script that is currently functioning correctly

---
+++ Unity Project/AINT354 Project/Assets/Scripts/KeyPickUp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPickUp : MonoBehaviour {
+
+
+    private bool playerWithinTrigger = false;
+    private MeshRenderer keyMesh;
+	// Use this for initialization
+	void Start () {
+        keyMesh = gameObject.GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown (KeyCode.E) && playerWithinTrigger == true)
+        {
+            keyMesh.enabled = false;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "childPlayer")
+        {
+            playerWithinTrigger = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "childPlayer")
+        {
+            playerWithinTrigger = false;
+        }
+    }
+}
---
+++ Unity Project/AINT354 Project/Assets/Scripts/SoundOnImpact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnImpact : MonoBehaviour {
+
+    private AudioSource soundToPlay;
+
+	// Use this for initialization
+	void Start () {
+        soundToPlay = gameObject.GetComponent<AudioSource>();
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            soundToPlay.Play();
+        }
+        
+    }
+}
